e4074471f05b3c262c52d8a2d95c951f231473dd
huutrungle2001 <huutrungle2001@gmail.com>
2021-11-14 02:29:30 +0700

    Read bmp, change RGB to HSV

--- cam_detect.c
+++ cam_detect.c
@@ -115,6 +115,26 @@ void displayCalibration()
         printCalibration(*(listCalibration + i));
 }
 
+void CalibrateColorProfile(char *filename){
+    Bmp bmp = read_bmp(filename);
+    int midX = bmp.width/2;
+    int midY = bmp.height/2;
+
+    HSV center[50][50];
+    for(int i = midY - 25, m = 0; i < midY + 25 && m < 50; i++, m++) {
+        for(int j = midX - 25, n = 0; j < midX + 25 && n < 50; j++, n++) {
+            center[m][n] = rgb2hsv(bmp.pixels[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 50; i++) {
+        for(int j = 0; j < 50; j++) {
+            printf("(%d %d %d) ", center[i][j].hue, center[i][j].saturation, center[i][j].value);
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char **argv)
 {
     if (checkInput(argc, argv) == false)
